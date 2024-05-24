818c65551ecda6d6b472b83ff7b0cc9236a9a206
Michael Thierry <michaelthierry86@gmail.com>
2024-05-24 06:34:57 -0400

    build: soluction 1011

---
+++ C/iniciante/sequencial/1011.c
@@ -0,0 +1,27 @@
+/**
+ * @file 1011.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Problema: 1011 - Esfera
+ * @version 0.1
+ * @date 2024-05-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    // variaveis
+    double const PI = 3.14159;
+    double raioEsfera, volumeEsfera;
+    // lendo do usuario
+    scanf("%lf", &raioEsfera);
+    // calculando o volume da esfera
+    volumeEsfera = ((4.0 / 3.0) * PI * (raioEsfera * raioEsfera * raioEsfera));
+    // exibindo resultado
+    printf("VOLUME = %.3lf\n", volumeEsfera);
+    // fim
+    return 0;
+}
