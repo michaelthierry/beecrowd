dc685ea25cecd1251f0ec67b7cb248157fd809d3
michaelthierry <michaelthierry86@gmail.com>
2024-12-28 08:39:16 -0400

    build: Solution 1000 in C

---
+++ Solucao_C/Iniciante/Sequencial/1000.c
@@ -0,0 +1,19 @@
+/**
+ * @file 1000.c
+ * @author michaelthierry (michaelthierry86@gmail.com)
+ * @brief 1000 - Hello World
+ * @version 0.1
+ * @date 2024-12-28
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello World!\n");
+
+    return 0;
+}
