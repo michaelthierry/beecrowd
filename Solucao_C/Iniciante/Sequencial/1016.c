0f3fca454e65c2b9e76f52c07c325c156b163749
michaelthierry <michaelthierry86@gmail.com>
2025-01-22 20:33:39 -0400

    build: soluction 1016 in C

---
+++ Solucao_C/Iniciante/Sequencial/1016.c
@@ -0,0 +1,26 @@
+/**
+ * @file 1016.c
+ * @author michaelthierry (michaelthierry86@gmail.com)
+ * @brief 1016 - Distância
+ * @version 0.1
+ * @date 2025-01-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+    // declarando variaveis
+    int kilometros, tempo;
+    // lendo do usuario
+    scanf("%d", &kilometros);
+    // calculando o tempo
+    tempo = 2 * kilometros;
+    // exibindoi resultado
+    printf("%d minutos\n", tempo);
+    // fim
+    return 0;
+
+}
