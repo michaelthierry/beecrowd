6368aff643ad0543d4c0917f37dcf951718ef646
michaelthierry <michaelthierry86@gmail.com>
2025-09-29 15:02:08 -0400

    build: solucao 1000

---
+++ language_C/iniciante/sequencial/1000.c
@@ -0,0 +1,19 @@
+/**
+ * @file 1000.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Hello World
+ * @version 0.1
+ * @date 2025-09-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World!\n");
+    
+    return 0;
+}
