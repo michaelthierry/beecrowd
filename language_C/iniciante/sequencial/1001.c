00789a5b24543813e1b6ed4a3d5c08b06431427b
michaelthierry <michaelthierry86@gmail.com>
2025-09-29 15:24:39 -0400

    build: solucao 1001

---
+++ language_C/iniciante/sequencial/1001.c
@@ -0,0 +1,26 @@
+/**
+ * @file 1001.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Extremamente Básico
+ * @version 0.1
+ * @date 2025-09-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    int primeiroValor, segundoValor, resultado;
+
+    scanf("%i", &primeiroValor);
+    scanf("%i", &segundoValor);
+
+    resultado = primeiroValor + segundoValor;
+    
+    printf("X = %i\n", resultado);
+
+    return 0;
+}
