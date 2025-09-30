126b2b936ae2beef59a73c73d3f8aab98dbaa602
michaelthierry <michaelthierry86@gmail.com>
2025-09-30 16:19:31 -0400

    build: solucao 1016

---
+++ language_C/iniciante/sequencial/1016.c
@@ -0,0 +1,24 @@
+/**
+ * @file 1016.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Distancia
+ * @version 0.1
+ * @date 2025-09-30
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+
+int main(){
+    
+    int distanciaKm, minutos;
+
+    scanf("%i", &distanciaKm);
+    
+    minutos = distanciaKm * 2;
+    
+    printf("%i minutos\n", minutos);
+
+    return 0;
+}
