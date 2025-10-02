e86c6684a9f40e565f8a4ef3b7e651073757864d
michaelthierry <michaelthierry86@gmail.com>
2025-10-01 21:20:28 -0400

    build:solucao 1005

---
+++ language_C++/iniciante/sequencial/1005.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file 1005.cpp
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Media 1
+ * @version 0.1
+ * @date 2025-10-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    
+    float primeiraNota, segundaNota, media;
+
+    std::cin >> primeiraNota >> segundaNota;
+
+    media = ((primeiraNota * 3.5) + (segundaNota * 7.5)) / 11.0;
+;
+    std::cout << "MEDIA = " << std::setprecision(5) << std::fixed << media << std:: endl;
+    
+    return 0;
+
+}
