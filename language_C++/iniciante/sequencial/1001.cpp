27c0257275377c508c21e3b37cb8a670a38356ad
michaelthierry <michaelthierry86@gmail.com>
2025-10-01 21:11:08 -0400

    build:solucao 1001

---
+++ language_C++/iniciante/sequencial/1001.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file 1001.cpp
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Extremamente Básico
+ * @version 0.1
+ * @date 2025-09-30
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+
+int main() {
+    int primeiroValor, segundoValor, resultado;
+
+    std::cin >> primeiroValor >> segundoValor;
+    resultado = primeiroValor + segundoValor;
+
+    std::cout << "X = " << resultado << std::endl;
+
+    return 0;
+}
