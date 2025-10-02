dc12a5c33962330f3cb305290cef5d35cb17ba34
michaelthierry <michaelthierry86@gmail.com>
2025-10-01 21:11:31 -0400

    build:solucao 1003

---
+++ language_C++/iniciante/sequencial/1003.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file 1003.cpp
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Soma Simples
+ * @version 0.1
+ * @date 2025-10-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+
+int main() {
+    int primeiroValor, segundoValor, soma;
+    
+    std::cin >> primeiroValor >> segundoValor;
+
+    soma = primeiroValor + segundoValor;
+
+    std::cout << "SOMA = " << soma << std::endl;
+
+    return 0;
+}
