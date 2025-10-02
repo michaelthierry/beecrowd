c94bedc0ac788821db6ee25ebd9ef0a78f9f94b9
michaelthierry <michaelthierry86@gmail.com>
2025-10-01 21:09:35 -0400

    build:solucao 1000

---
+++ language_C++/iniciante/sequencial/1000.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file 1000.cpp
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Hello World!
+ * @version 0.1
+ * @date 2025-09-30
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
