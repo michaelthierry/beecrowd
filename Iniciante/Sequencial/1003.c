0168bbf6b2af9f9ac255223059850a9e11cb4565
michaelthierry <michaelthierry86@gmail.com>
2026-05-23 16:35:17 -0400

    build: solucao 1003 em C

---
+++ Iniciante/Sequencial/1003.c
@@ -0,0 +1,25 @@
+/**
+ * @file 1003.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Soma Simples
+ * @version 0.1
+ * @date 2025-09-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+
+int main() {
+    
+    int primeiroValor, segundoValor, soma;
+    
+    scanf("%d", &primeiroValor);
+    scanf("%d", &segundoValor);
+    
+    soma = primeiroValor + segundoValor;
+    
+    printf("SOMA = %d\n", soma);
+    
+    return 0;
+}
