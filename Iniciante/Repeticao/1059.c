1ebdc1a8b7ff2988299031cdf979404b92c47bda
michaelthierry <michaelthierry86@gmail.com>
2026-05-30 02:12:19 -0400

    build: solucao 1059 em C

---
+++ Iniciante/Repeticao/1059.c
@@ -0,0 +1,20 @@
+/**
+ * @file 1059.c
+ * @author Michael Thierry (michaelthierry86@gmail.com)
+ * @brief Numeros Pares
+ * @version 0.1
+ * @date 2026-05-30
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include <stdio.h>
+
+int main() {
+    
+    for(int indice = 2; indice <= 100; indice += 2) {
+        printf("%i\n", indice);
+    }
+
+    return 0;
+}
